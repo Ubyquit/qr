05bb083cd2a440318863c49a68e6009ab30b7bf7
Ubyquit <ingsystem.out.print@gmail.com>
2023-04-05 11:46:59 -0500

    style

--- script.js
+++ script.js
@@ -1,40 +1,43 @@
-const qrCodeEl = document.getElementById('qrcode');
-const generateBtn = document.getElementById('generate-btn');
-const downloadBtn = document.getElementById('download-btn');
+const qrCodeEl = document.getElementById("qrcode");
+const generateBtn = document.getElementById("generate-btn");
+const downloadBtn = document.getElementById("download-btn");
+const colorInput = document.getElementById("color-input");
 
 function downloadQRCode() {
-  const qrCodeDataUrl = qrCodeEl.querySelector('img').src;
-  const downloadLink = document.createElement('a');
+  const qrCodeDataUrl = qrCodeEl.querySelector("img").src;
+  const downloadLink = document.createElement("a");
   downloadLink.href = qrCodeDataUrl;
-  downloadLink.download = 'qr-code.png';
+  downloadLink.download = "qr-code.png";
   document.body.appendChild(downloadLink);
   downloadLink.click();
   document.body.removeChild(downloadLink);
 }
 
-function generateQRCode(url) {
+function generateQRCode(url, color) {
+  qrCodeEl.innerHTML = ""; // Borramos cualquier QR previo
   if (url) {
     // Utilizamos la librería qrcode.js para generar el código QR
     const qrCode = new QRCode(qrCodeEl, {
       text: url,
       width: 500,
       height: 500,
-      colorDark: '#000000',
-      colorLight: '#ffffff',
-      correctLevel: QRCode.CorrectLevel.H
+      colorDark: color,
+      colorLight: "#ffffff",
+      correctLevel: QRCode.CorrectLevel.H,
     });
     downloadBtn.disabled = false;
   } else {
-    qrCodeEl.innerHTML = '';
+    qrCodeEl.innerHTML = "";
     downloadBtn.disabled = true;
   }
 }
 
-generateBtn.addEventListener('click', () => {
-  const url = document.getElementById('url').value;
-  generateQRCode(url);
+generateBtn.addEventListener("click", () => {
+  const url = document.getElementById("url").value;
+  const color = colorInput.value.toString();
+  generateQRCode(url, color);
 });
 
-downloadBtn.addEventListener('click', () => {
+downloadBtn.addEventListener("click", () => {
   downloadQRCode();
 });
